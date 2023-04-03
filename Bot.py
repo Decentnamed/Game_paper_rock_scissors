eb233a4c241d875e3000d41c79e430ac13631a20
DawidG <dawid.grabowicz20@gmail.com>
2023-04-03 17:27:43 +0200

    init commit with basic functionality of the game

---
+++ Bot.py
@@ -0,0 +1,9 @@
+# class for bot playing
+import random
+from Player import *
+
+#Class for Bot inherit from Player class
+class Bot(Player):
+    # overload method
+    def select_option(self):
+        return random.choice(["paper", "rock", "scissors"])    
---
+++ Player.py
@@ -0,0 +1,29 @@
+#Class for typing by player
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+    
+    # method for option select by Player
+    def select_option(self):
+        while True:
+            option = input(f"{self.name}, Type option: paper, rock, scissors: ")
+            if option == "paper":
+                return option
+            elif option == "rock":
+                return option
+            elif option == "scissors":
+                return option
+            else:
+                print("wrong type, try again")
+
+    def add_score(self):
+        self.score += 1
+
+    def get_name(self):
+        return self.name
+
+    def get_score(self):
+        return self.score
+                
